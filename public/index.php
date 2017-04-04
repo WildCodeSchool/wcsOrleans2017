7465f4aa360ccc6a3fc583409084c913783b33de
nico <nicolas@wildcodeschool.fr>
2017-04-04 09:49:44 +0200

    correction index

--- public/index.php
+++ public/index.php
@@ -3,7 +3,8 @@
 require '../config/connect.php';
 require __DIR__ . '/../vendor/autoload.php';
 
-$route = $_GET['route'];
+$route = $_GET['route'] ?? 'listEleve';
+$render = '';
 
 if ($route == 'listEleve') {
     $eleve = new \wcs\controller\EleveController();
@@ -12,15 +13,12 @@ if ($route == 'listEleve') {
 } elseif ($route == 'showEleve') {
     $eleve = new \wcs\controller\EleveController();
     $render = $eleve->show($_GET['id']);
-}  else {
-    header('Location: index.php');
+} elseif ($route == 'addEleve') {
+    $eleve = new \wcs\controller\EleveController();
+    $render = $eleve->add();
 }
+
 
 require '../src/view/partial/header.php';
 echo $render;
 require '../src/view/partial/footer.php';
-
-?>
-
-
-
