bcdcbc4a74c4fc487b504a7a5a1251a432ae1053
nico <nicolas@wildcodeschool.fr>
2017-04-04 09:50:51 +0200

    correction lien ajout eleve

--- src/view/eleve/listAllEleve.php
+++ src/view/eleve/listAllEleve.php
@@ -1,26 +1,26 @@
 <h2>Trombinoscope</h2>
-    <div class="row">
+<div class="row">
     <?php foreach ($eleves as $eleve) : ?>
-    <div class="col-sm-4">
-        <div class="thumbnail">
-            <img src="http://placehold.it/200x200" alt="...">
-            <div class="caption">
-                <h3><a href="../public/index.php?route=showEleve&id="<?= $eleve->getId() ?> ">
+        <div class="col-sm-4">
+            <div class="thumbnail">
+                <img src="http://placehold.it/200x200" alt="...">
+                <div class="caption">
+                    <h3><a href="../public/index.php?route=showEleve&id="<?= $eleve->getId() ?> ">
                         <?= $eleve->getCivilite().' '.$eleve->getPrenom().' '.$eleve->getNom() ?>
-                    </a>
-                </h3>
-                <p><?= $eleve->getType() ?></p>
-                <form method="POST" action="deleteEleve.php">
-                    <input type="hidden" name="id" value="<?= $eleve->getId() ?>"/>
-                    <input  class="btn btn-danger" type="submit" value="Delete" name="delete"/>
-                </form>
+                        </a>
+                    </h3>
+                    <p><?= $eleve->getType() ?></p>
+                    <form method="POST" action="deleteEleve.php">
+                        <input type="hidden" name="id" value="<?= $eleve->getId() ?>"/>
+                        <input  class="btn btn-danger" type="submit" value="Delete" name="delete"/>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <?php
-    ?>
-<?php endforeach ?>
+        <?php
+        ?>
+    <?php endforeach ?>
 
 </div>
 
-<a href="ficheEleve.php" class="btn btn-primary">Ajouter un élève</a>
+<a href="index.php?route=addEleve" class="btn btn-primary">Ajouter un élève</a>
